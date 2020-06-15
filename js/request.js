8dacbbebd85c88f6f2e9a227a26ec49e57503035
Pradeep Malviya <35966390+pradeepmalvi@users.noreply.github.com>
2020-06-15 23:28:12 +0530

    Changes in api key

--- js/request.js
+++ js/request.js
@@ -1,31 +1,37 @@
-class RequestAPI{
-    async getNews(){
-        const url = await fetch('https://newsapi.org/v2/top-headlines?country=in&apiKey=dad7070b5eeb44208ef89a3ae945f818');
+class RequestAPI {
+  async getNews() {
+    const url = await fetch(
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=44777051b4b0429fb1be37670c300ec7"
+    );
 
-        const news = await url.json();
+    const news = await url.json();
 
-        return{
-            news
-        }
-    }
+    return {
+      news
+    };
+  }
 
-    async getNewsByCategory(category){
-        const url = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=dad7070b5eeb44208ef89a3ae945f818`);
+  async getNewsByCategory(category) {
+    const url = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=44777051b4b0429fb1be37670c300ec7`
+    );
 
-        const news = await url.json();
+    const news = await url.json();
 
-        return{
-            news
-        }
-    }
+    return {
+      news
+    };
+  }
 
-    async getNewsByName(name){
-        const url = await fetch(`https://newsapi.org/v2/top-headlines?sources=${name}&apiKey=dad7070b5eeb44208ef89a3ae945f818`);
+  async getNewsByName(name) {
+    const url = await fetch(
+      `https://newsapi.org/v2/top-headlines?sources=${name}&apiKey=44777051b4b0429fb1be37670c300ec7`
+    );
 
-        const news = await url.json();
+    const news = await url.json();
 
-        return{
-            news
-        }
-    }
+    return {
+      news
+    };
+  }
 }
